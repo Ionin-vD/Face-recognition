81090ef6c3d5fa9dd6e5c96ed57089833bf74297
Дмитрий <dmitrijionin@gmail.com>
2023-03-19 12:46:05 +0300

    one_versions

--- main1.py
+++ main1.py
@@ -1,59 +1,51 @@
 # -*- coding: utf-8 -*-
 import sqlite3
-import tkinter
 import tkinter.messagebox as mb
 from tkinter import messagebox
-
 import cv2
 import os
-
 import bdmain
 # пути прописываем вручную, path_s - откуда, path_d - куда
 import checkImgBd
 import comparison
 
 
+# пути прописываем вручную, path_s - откуда, path_d - куда
 def import_img():
     path_s = r'D:/PythonProject/projectHackathon/'
     path_d = r'D:/PythonProject/projectHackathon/tmp/'
     for file in os.listdir(path_s):
-        if file.endswith(".png") or file.endswith(".jpg"):
+        if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
             os.replace(path_s + file, path_d + file)
 
 
 def main():
-    msg = "Нажмите пробел для сохранения фото. \'q\' для выхода"
-    mb.showinfo("Инфо.", msg)
-
-    face_cascade_db = cv2.CascadeClassifier('Face.xml')
-    cap = cv2.VideoCapture(0)
+    face_cascade_db = cv2.CascadeClassifier("Face.xml")
+    cap=cv2.VideoCapture(0)
     img_counter = 0
     while True:
         success, img = cap.read()
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.rectangle(img, (x, y), (x+w, y+h),(255,0,0),2 )
         cv2.imshow('vas zametili', img)
         ret, frame = cap.read()
         if not ret:
-            # print("failed to grab frame")
+            #print("failed to grab frame")
             break
         k = cv2.waitKey(1)
         if k % 256 == 81 or k % 256 == 113 or k % 256 == 201 or k % 256 == 233:
             # й или q  pressed
             # print("Escape hit, closing...")
-
             break
         elif k % 256 == 32:
             # SPACE pressed
             img_name = "opencv_frame_{}.png".format(img_counter)
             cv2.imwrite(img_name, frame)
             img_counter += 1
-
     cap.release()
     cv2.destroyAllWindows()
-
     import_img()
 
     conn = sqlite3.connect('Base.db')
